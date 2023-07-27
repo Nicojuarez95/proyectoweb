854732ad92bcabdcf99de58d1d92c446935f6c09
Nico <juareznico61@gmail.com>
2023-07-27 19:29:48 -0300

    pag proyecto web

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../src/Img/favicon.png';
 import './App.css';
 
 function App() {
@@ -6,18 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <main>
+        <section className='section1'>
+
+        </section>
+        <section className='section2'>
+
+        </section>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
